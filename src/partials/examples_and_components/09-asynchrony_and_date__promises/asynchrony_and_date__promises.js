7911ed123868f2fe26f647d3607b9437499c8ec1
ValeriyVinn <valeriy.all@gmail.com>
2024-04-09 18:25:41 +0300

    Added timer

--- src/partials/examples_and_components/09-asynchrony_and_date__promises/asynchrony_and_date__promises.js
+++ src/partials/examples_and_components/09-asynchrony_and_date__promises/asynchrony_and_date__promises.js
@@ -1,29 +1,154 @@
-//! Сповіщення
-const NOTIFICATION_DELAY = 3000;
-let timeoutId = null;
+// import BSN from 'bootstrap.native';
 
-const refsNotification = {
-  notification: document.querySelector('.js-alert'),
+
+
+//! Notification ------------
+
+// const NOTIFICATION_DELAY = 3000;
+// let timeoutId = null;
+
+// const refsNotification = {
+//   notification: document.querySelector('.js-alert'),
+// };
+
+// refsNotification.notification.addEventListener('click', onNotificationClick);
+
+// showNotification();
+
+// function onNotificationClick() {
+//   hideNotification();
+//   clearTimeout(timeoutId);
+// }
+
+// function showNotification() {
+//   refsNotification.notification.classList.add('is-visible');
+
+//   timeoutId = setTimeout(() => {
+//     console.log('qerty');
+//     hideNotification();
+//   }, NOTIFICATION_DELAY);
+// }
+
+// function hideNotification() {
+//   refsNotification.notification.classList.remove('is-visible');
+// }
+
+//! Annoying ------------------
+
+// const refs = {
+//   modal: document.querySelector('#subscription-modal'),
+//   subscribeBtn: document.querySelector('button[data-subscribe]'),
+// };
+// const PROMPT_DELAY = 3000;
+// const MAX_PROMPT_ATTEMPTS = 3;
+// let promptCounter = 0;
+// let hasSubscribed = false;
+// const modal = new BSN.Modal('#subscription-modal');
+
+// openModal();
+
+// refs.modal.addEventListener('hide.bs.modal', openModal);
+// refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
+
+// function openModal() {
+//   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
+//     console.log('Максимальное кол-во надоеданий или подписался');
+//     return;
+//   }
+
+//   setTimeout(() => {
+//     console.log('Открываем надоедалку');
+//     modal.show();
+//     promptCounter += 1;
+//   }, PROMPT_DELAY);
+// }
+
+// function onSubscribeBtnClick() {
+//   hasSubscribed = true;
+//   modal.hide();
+// }
+// ! ------------------
+
+const refs = {
+  startBtn: document.querySelector('button[data-action-start]'),
+  stopBtn: document.querySelector('button[data-action-stop]'),
+  clockface: document.querySelector('.js-clockface'),
 };
 
-refsNotification.notification.addEventListener('click', onNotificationClick);
+class Timer {
+  constructor({ onTick }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
 
-showNotification();
+    this.init();
+  }
 
-function onNotificationClick() {
-  hideNotification();
-  clearTimeout(timeoutId);
+  init() {
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    const startTime = Date.now();
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.onTick(time);
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
+  }
+
+  /*
+   * - Принимает время в миллисекундах
+   * - Высчитывает сколько в них вмещается часов/минут/секунд
+   * - Возвращает обьект со свойствами hours, mins, secs
+   * - Адская копипаста со стека 💩
+   */
+  getTimeComponents(time) {
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { hours, mins, secs };
+  }
+
+  /*
+   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
+   */
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
 }
 
-function showNotification() {
-  refsNotification.notification.classList.add('is-visible');
+const timer = new Timer({
+  onTick: updateClockface,
+});
 
-  timeoutId = setTimeout(() => {
-    console.log('qerty');
-    hideNotification();
-  }, NOTIFICATION_DELAY);
-}
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
 
-function hideNotification() {
-  refsNotification.notification.classList.remove('is-visible');
+/*
+ * - Принимает время в миллисекундах
+ * - Высчитывает сколько в них вмещается часов/минут/секунд
+ * - Рисует интерфейс
+ */
+function updateClockface({ hours, mins, secs }) {
+  refs.clockface.textContent = `${hours}:${mins}:${secs}`;
 }
